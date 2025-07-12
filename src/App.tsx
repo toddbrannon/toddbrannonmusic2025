059f7294478930adc36c547f5b3b02701e9ad372
toddbrannon <toddqbrannon@gmail.com>
2025-07-11 20:44:40 -0500

    Add new 'Live Performance' video link

--- src/App.tsx
+++ src/App.tsx
@@ -94,6 +94,7 @@ function App() {``
   const livePerformances = [
     { id: 'HJlMHuzPDKY', title: 'All I Can Say - Valley Creek Worship' },
     { id: 'M5SHz--FuVg', title: 'This Is Love - Valley Creek Worship' },
+    { id: 'YPzODruk1lHr359j&t', title: 'Light of the World - Valley Creek Christmas 2024'}
   ];
 
   useEffect(() => {
